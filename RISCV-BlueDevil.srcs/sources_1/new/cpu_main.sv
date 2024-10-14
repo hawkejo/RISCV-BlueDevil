a7f71f0d8238bc5aee68eb92a186bc2495e046d4
Joseph H <hawkejo@users.noreply.github.com>
2024-10-14 11:57:07 -0400

    Initial commit

---
+++ RISCV-BlueDevil.srcs/sources_1/new/cpu_main.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/14/2024 11:55:05 AM
+// Design Name: 
+// Module Name: cpu_main
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module cpu_main(
+
+    );
+endmodule
---
+++ RISCV-BlueDevil.srcs/sources_1/new/nexys4ddr_interface.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/14/2024 11:55:05 AM
+// Design Name: 
+// Module Name: nexys4ddr_interface
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module nexys4ddr_interface(
+
+    );
+endmodule
